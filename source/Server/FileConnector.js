e8d5711512982fb82e0d6cfc98d8d02f56fe8e9f
Tobias Kempkensteffen <tobias.kempkensteffen@gmail.com>
2012-10-04 16:01:38 +0200

    koalaConnector, koalaAPI, ...

--- source/Server/FileConnector.js
+++ source/Server/FileConnector.js
@@ -67,35 +67,6 @@ fileConnector.login=function(username,password,rp){
 *	 RIGHTS
 */
 
-fileConnector.mayRead=function(roomID,objectID,context){
-	if (!context) throw new Error('Missing context in fileConnector.mayRead');
-	return true;	
-}
-
-fileConnector.mayWrite=function(roomID,objectID,context){
-	if (!context) throw new Error('Missing context in fileConnector.mayWrite');
-	return true;	
-}
-
-fileConnector.mayEvaluate=function(roomID,objectID,context){
-	if (!context) throw new Error('Missing context in fileConnector.mayEvaluate');
-	return true;
-}
-
-fileConnector.mayDelete=function(roomID,objectID,context){
-	if (!context) throw new Error('Missing context in fileConnector.mayDelete');
-	return true;	
-}
-
-fileConnector.mayCreate=function(roomID,type,context){
-	if (!context) throw new Error('Missing context in fileConnector.mayCreate');
-	return true;
-}
-
-fileConnector.maySubscribe=function(room,context){
-	if (!context) throw new Error('Missing context in fileConnector.maySubscribe');
-	return true;
-}
 
 fileConnector.getUserRightsForObjects=function(roomID, ids, context, callback) {
 	
@@ -251,6 +222,30 @@ fileConnector.saveContent=function(roomID,objectID,content,after,context){
 }
 
 
+
+/**
+*	getContent
+*
+*	get an object's content as a an array of bytes;
+*	
+*/
+fileConnector.getContent=function(roomID,objectID,context,callback){
+	
+	if (!context) throw new Error('Missing context in fileConnector.getContent');
+	
+	var filebase=Modules.Config.filebase;
+	
+	var filename=filebase+'/'+roomID+'/'+objectID+'.content';
+	
+	try {
+		var content = fs.readFileSync(filename, 'utf8');
+		callback(content);
+	} catch (e) {
+		callback(false);
+	}
+}
+
+
 /**
 *	get the the object's content from a file and save it
 *
@@ -280,27 +275,6 @@ fileConnector.copyContentFromFile=function(roomID, objectID, sourceFilename, cal
 	
 }
 
-/**
-*	getContent
-*
-*	get an object's content as a an array of bytes;
-*	
-*/
-fileConnector.getContent=function(roomID,objectID,context){
-	
-	if (!context) throw new Error('Missing context in fileConnector.getContent');
-	
-	var filebase=Modules.Config.filebase;
-	
-	var filename=filebase+'/'+roomID+'/'+objectID+'.content';
-	
-	try {
-		var content = fs.readFileSync(filename, 'utf8');
-		return content;
-	} catch (e) {
-		return false;
-	}
-}
 
 
 
@@ -447,362 +421,6 @@ fileConnector.duplicateObject=function(roomID,objectID,callback,context){
 }
 
 
-fileConnector.trimImage=function(roomID, objectID, callback, context) {
-	
-	if (!context) throw new Error('Missing context in fileConnector.trimImage');
-
-	var im = require('imagemagick');
-	
-	var filebase=Modules.Config.filebase;
-	
-	var filename=filebase+'/'+roomID+'/'+objectID+'.content';
-	
-	//output: test.png PNG 192x154 812x481+226+131 8-bit DirectClass 0.010u 0:00.000
-	im.convert([filename, '-trim', 'info:-'], function(err,out,err2) {
-		
-		if (!err) {
-
-			var results = out.split(" ");
-
-			var dimensions = results[2];
-			var dimensionsA = dimensions.split("x");
-			
-			var newWidth = dimensionsA[0];
-			var newHeight = dimensionsA[1];
-			
-			var d = results[3];
-			var dA = d.split("+");
-			
-			var dX = dA[1];
-			var dY = dA[2];
-
-			im.convert([filename, '-trim', filename], function(err,out,err2) {
-
-				if (!err) {
-
-					callback(dX, dY, newWidth, newHeight);
-
-				} else {
-					console.log("trimImage: error while trimming "+roomID+"/"+objectID);
-					callback(false);
-				}
-
-			});
-			
-		} else {
-			console.log("trimImage: error getting trim information of "+roomID+"/"+objectID);
-			callback(false);
-		}
-	
-	});
-	
-};
-
-
-fileConnector.isInlineDisplayable=function(mimeType) {
-	
-	if (this.getInlinePreviewProviderName(mimeType) == false) {
-		return false;
-	} else {
-		return true;
-	}
-	
-}
-
-fileConnector.getMimeType=function(roomID,objectID,context) {
-	
-	if (!context) throw new Error('Missing context in fileConnector.getMimeType');
-	
-	var filebase=Modules.Config.filebase;
-	
-	var filename=filebase+'/'+roomID+'/'+objectID+'.object.txt';
-	
-	try {
-		var attributes = fs.readFileSync(filename, 'utf8');
-		attributes=JSON.parse(attributes);
-	} catch (e) {
-		console.log("fileConnector.getInlinePreviewDimensions: unable to read file '"+filename+"'");
-		return false;
-	}
-	
-	var mimeType = attributes.mimeType;
-	
-	if (!mimeType) {
-		console.log("fileConnector.getMimeType: unable to read mimeType of object '"+objectID+"'");
-		return false;
-	}
-	
-	if (!this.isInlineDisplayable(mimeType)) {
-		console.log("fileConnector.getMimeType: object '"+objectID+"' with mime type '"+mimeType+"' is not inline displayable!");
-		return false;
-	}
-	
-	return mimeType;
-	
-}
-
-fileConnector.getInlinePreviewProviderName=function(mimeType) {
-
-	if (!mimeType) return false;
-
-	if (this.getInlinePreviewProviders()[mimeType] != undefined) {
-		return this.getInlinePreviewProviders()[mimeType];
-	} else {
-		return false;
-	}
-	
-}
-
-fileConnector.getInlinePreviewMimeTypes=function() {
-	
-	var mimeTypes = this.getInlinePreviewProviders();
-	var list = {};
-	
-	for (var mimeType in mimeTypes){
-		list[mimeType] = true;
-	}
-	
-	return list;
-	
-}
-
-fileConnector.getInlinePreviewProviders=function() {
-	return {
-		"application/pdf" : "pdf",
-		"image/jpeg" : "image",
-		"image/png" : "image",
-		"image/gif" : "image"
-	}
-}
-
-fileConnector.getInlinePreviewDimensions=function(roomID, objectID, callback, mimeType,context) {
-	
-	if (!context) throw new Error('Missing context in fileConnector.getInlinePreviewDimensions');
-	
-	if (!mimeType) {
-		var mimeType = fileConnector.getMimeType(roomID,objectID);
-	}
-	
-	/* find provider for inline content: */
-	var generatorName = this.getInlinePreviewProviderName(mimeType);
-	
-	if (generatorName == false) {
-		console.log("getInlinePreviewDimensions: no generator name for mime type '"+mimeType+"' found!");
-		callback(false, false); //do not set width and height (just send update to clients)
-	} else {
-		this.inlinePreviewProviders[generatorName].dimensions(roomID, objectID, callback, context);
-	}
-	
-}
-
-fileConnector.getInlinePreview=function(roomID, objectID, callback, mimeType,context) {
-	
-	if (!context) throw new Error('Missing context in fileConnector.getInlinePreview');
-	
-	if (!mimeType) {
-		var mimeType = fileConnector.getMimeType(roomID,objectID,context);
-	}
-	
-	if (!mimeType) {
-		return false;
-		callback(false);
-	}
-	
-	/* find provider for inline content: */
-	var generatorName = this.getInlinePreviewProviderName(mimeType);
-
-	if (generatorName == false) {
-		console.log("getInlinePreview: no generator name for mime type '"+mimeType+"' found!");
-		callback(false); //do not set width and height (just send update to clients)
-	} else {
-		this.inlinePreviewProviders[generatorName].preview(roomID, objectID, callback, context);
-	}
-	
-}
-
-fileConnector.getInlinePreviewMimeType=function(roomID, objectID,context) {
-	
-	if (!context) throw new Error('Missing context in fileConnector.getInlinePreviewMimeType');
-	
-	var mimeType = fileConnector.getMimeType(roomID,objectID,context);
-	
-	if (!mimeType) {
-		return false;
-	}
-	
-	/* find provider for inline content: */
-	var generatorName = this.getInlinePreviewProviderName(mimeType);
-	
-	if (generatorName == false) {
-		console.log("getInlinePreviewMimeType: no generator name for mime type '"+mimeType+"' found!");
-		return false;
-	} else {
-		return this.inlinePreviewProviders[generatorName].mimeType(roomID, objectID, mimeType, context);
-	}
-	
-}
-
-fileConnector.inlinePreviewProviders = {
-	
-	'image': {
-		'mimeType' : function(roomID, objectID, mimeType, context) {
-			
-			if (!context) throw new Error('Missing context in mimeType for image');
-			
-			return mimeType;
-		},
-		'dimensions' : function(roomID, objectID, callback, context) {
-			
-			if (!context) throw new Error('Missing context in dimensions for image');
-
-			var filebase=Modules.Config.filebase;
-
-			var filename=filebase+'/'+roomID+'/'+objectID+'.content';
-			
-
-			var im = require('imagemagick');
-
-			im.identify(filename, function(err, features) {
-
-				if (err) throw err;
-				
-				var width = features.width;
-				var height = features.height;
-
-				if (width > Modules.config.imageUpload.maxDimensions) {
-					height = height*(Modules.config.imageUpload.maxDimensions/width);
-					width = Modules.config.imageUpload.maxDimensions;
-				}
-
-				if (height > Modules.config.imageUpload.maxDimensions) {
-					width = width*(Modules.config.imageUpload.maxDimensions/height);
-					height = Modules.config.imageUpload.maxDimensions;
-				}
-
-				callback(width, height);
-			
-			});
-
-		},
-		'preview' : function(roomID, objectID, webserverResponse, context) {
-			
-			if (!context) throw new Error('Missing context in preview for image');
-
-			var filebase=Modules.Config.filebase;
-
-			var filename=filebase+'/'+roomID+'/'+objectID+'.content';
-
-			try {
-				var content = fs.readFileSync(filename);
-				webserverResponse.write(content);
-				webserverResponse.end();
-			} catch (e) {
-				webserverResponse.end("error!");
-			}
-
-		}
-	},
-	
-	
-	'pdf': {
-		'mimeType' : function(roomID, objectID, mimeType, context) {
-			
-			if (!context) throw new Error('Missing context in mimeType for pdf');
-			
-			return 'image/jpeg';
-		},
-		'generatePreviewFile' : function(roomID, objectID, callback, context) {
-			
-			if (!context) throw new Error('Missing context in generatePreviewFile for pdf');
-
-			var filebase=Modules.Config.filebase;
-
-			var filename = filebase+'/'+roomID+'/'+objectID+'.content';
-			var filenamePreview = filebase+'/'+roomID+'/'+objectID+'.preview';
-			
-
-			var im = require('imagemagick');
-			
-			im.convert(['-density', '200x200', 'pdf:'+filename+'[0]', '-quality', '100', 'jpg:'+filenamePreview], 
-			function(err, metadata){
-			  	if (err) {
-					console.log("error creating preview for pdf");
-					callback(false);
-				} else {
-
-					try {
-						var content = fs.readFileSync(filenamePreview);
-						callback(content);
-					} catch (e) {
-						callback(false);
-					}
-					
-				}
-			});
-	
-		},
-		'dimensions' : function(roomID, objectID, callback, context) {
-			
-			if (!context) throw new Error('Missing context in dimensions for pdf');
-
-			var filebase=Modules.Config.filebase;
-
-			var filename=filebase+'/'+roomID+'/'+objectID+'.content';
-			
-
-			var im = require('imagemagick');
-
-			im.identify(filename, function(err, features) {
-
-				if (err) throw err;
-				
-				var width = features.width;
-				var height = features.height;
-
-				if (width > Modules.config.imageUpload.maxDimensions) {
-					height = height*(Modules.config.imageUpload.maxDimensions/width);
-					width = Modules.config.imageUpload.maxDimensions;
-				}
-
-				if (height > Modules.config.imageUpload.maxDimensions) {
-					width = width*(Modules.config.imageUpload.maxDimensions/height);
-					height = Modules.config.imageUpload.maxDimensions;
-				}
-
-				callback(width, height);
-			
-			});
-
-		},
-		'preview' : function(roomID, objectID, callback, context) {
-			
-			if (!context) throw new Error('Missing context in preview for pdf');
-
-			var filebase=Modules.Config.filebase;
-
-			var filename=filebase+'/'+roomID+'/'+objectID+'.preview';
-
-			var path = require('path');
-
-			if (!path.existsSync(filename)) {
-				this.generatePreviewFile(roomID, objectID, function(data) {
-					/* preview file is generated */
-					callback(data);
-				});
-			} else {
-				/* preview file exists */
-				try {
-					var content = fs.readFileSync(filename);
-					callback(content);
-				} catch (e) {
-					callback(false);
-				}
-			}
-
-		}
-	}
-	
-}
 
 /**
 *	internal
