d41212314aeea1a847b71b790fd45e351161b62d
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2013-01-18 15:11:03 +0100

    Trying to fix SimpleKeyword

--- source/objects/5.SimpleKeyword/view.js
+++ source/objects/5.SimpleKeyword/view.js
@@ -14,36 +14,16 @@ SimpleKeyword.draw=function(external){
 	//$(rep).attr("fill", this.getAttribute('fillcolor'));
 	
 	if (!$(rep).hasClass("selected")) {
-		$(rep).attr("stroke", this.getAttribute('linecolor'));
-		$(rep).attr("stroke-width", this.getAttribute('linesize'));
+		$(rep).find("text").attr("stroke", this.getAttribute('linecolor'));
+		$(rep).find("text").attr("stroke-width", this.getAttribute('linesize'));
 	}
 	
-	$(rep).attr("font-size", this.getAttribute('font-size'));
-	$(rep).attr("font-family", this.getAttribute('font-family'));
-	$(rep).attr("fill", this.getAttribute('font-color'));
+	$(rep).find("text").attr("font-size", this.getAttribute('font-size'));
+	$(rep).find("text").attr("font-family", this.getAttribute('font-family'));
+	$(rep).find("text").attr("fill", this.getAttribute('font-color'));
 	
 	$(rep).attr("layer", this.getAttribute('layer'));
-
-
-	var that=this;
 	
-	rep.textContent=this.getAttribute('text');
+	$(rep).find("text").get(0).textContent=this.getAttribute('text');
 
 }
-
-
-
-SimpleKeyword.createRepresentation = function() {
-	
-	var rep = GUI.svg.text(10, 10, "Text");
-
-	rep.dataObject=this;
-
-	$(rep).attr("id", this.getAttribute('id'));
-
-	this.initGUI(rep);
-	
-	return rep;
-	
-}
-
