2ae8611964a1a59858d3ddc35f11bfffa2f929b8
Viktor Koop <viktor.koop@googlemail.com>
2013-02-19 11:00:50 +0100

    open file if clicked on it

--- source/objects/2.File/common.js
+++ source/objects/2.File/common.js
@@ -101,7 +101,7 @@ File.execute=function(){
 	var that=this;
 	
 	if (this.hasContent() == true) {
-		//this.openFile();
+		this.openFile();
 	} else {
 		GUI.uploadFile(this,this.translate(GUI.currentLanguage, "Please select a file"));
 	}
