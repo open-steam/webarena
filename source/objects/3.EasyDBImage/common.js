05b7f9913572f8d21d1827cc2471c4b63cbe9867
Viktor Koop <viktor.koop@googlemail.com>
2013-02-19 13:28:47 +0100

    fix meta data

--- source/objects/3.EasyDBImage/common.js
+++ source/objects/3.EasyDBImage/common.js
@@ -32,7 +32,7 @@ EasyDBImage.register=function(type){
     }
 
     this.registerAttribute('easydbtitel',{type:'metadata', category:'Meta Data',  getFunction : notNull("easydbtitel")});
-    this.registerAttribute('easydbkuenstler',{ctype:'metadata', ategory:'Meta Data',  getFunction : notNull("easydbkuenstler")});
+    this.registerAttribute('easydbkuenstler',{type:'metadata', category:'Meta Data',  getFunction : notNull("easydbkuenstler")});
 
     this.registerAttribute('easydbstandort',{type:'metadata', category:'Meta Data',  getFunction: notNull("easydbstandort")});
     this.registerAttribute('easydbdargestellter_ort',{type:'metadata', category:'Meta Data',  getFunction : notNull("easydbdargestellter_ort")});
