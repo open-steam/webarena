fe4e1ad090f5913182bee49ec4d8d12f6bb3cde1
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2013-04-10 12:10:33 +0200

    Made devoper info switchable

--- source/Server/config.default.js
+++ source/Server/config.default.js
@@ -27,6 +27,7 @@ module.exports={
 		"debug" : false,
 		"info" : false
 	},
+	developerInfo: false,
 	objectWhitelist: [],	//an empty whitelist whitelists everything
 	objectBlacklist: ['HtmlTest'],    //Syntax: objectBlacklist:['SharePoint','EasyDBImage']
 	noContexts: false
