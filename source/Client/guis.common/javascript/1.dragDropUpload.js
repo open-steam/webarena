9bcfe4e16ef1aee8f4892852fe0eac7c05bcf8f1
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2013-04-11 02:22:25 +0200

    Fixed multiple drag&drop file upload, works with many files now. Name and mimeType are set correctly.

--- source/Client/guis.common/javascript/1.dragDropUpload.js
+++ source/Client/guis.common/javascript/1.dragDropUpload.js
@@ -22,62 +22,71 @@ $(function() {
 		/* get dropped files */
 		var files = event.dataTransfer.files;
 		
+		var upload=function(file){
+			
+				
+		var progressBarId = GUI.progressBarManager.addProgress(GUI.translate("Create file object"));
+		
+		var filename = file.name;
+		var mimeType = file.type;
+
+		/* create new File object and set position */
+		ObjectManager.createObject("File",{
+			"x":x,
+			"y":y,
+			"hasContent":true //prevent calling justCreated() after object creation (would display file upload dialog)
+		},false,function(newObject) {
+			/* object created --> upload content */
+			
+			var fd=new FormData();
+			fd.append("file", file); // Append the file
+			
+			var xhr = new XMLHttpRequest();
+			
+			xhr.upload.addEventListener("progress",function(evt) {
+				if (evt.lengthComputable){
+					
+					var percentComplete=Math.round(evt.loaded * 100 / evt.total);
+					GUI.progressBarManager.updateProgress(progressBarId, percentComplete);
+					
+				}
+			}, false);
+			
+			xhr.addEventListener("load",function() {
+				GUI.progressBarManager.updateProgress(progressBarId, 100, GUI.translate("Upload completed"), true);
+			},false);
+			
+			xhr.addEventListener("error",function() {
+				GUI.progressBarManager.error(progressBarId, GUI.translate("Error while uploading file"));
+			},false);
+			
+			xhr.addEventListener("abort",function() {
+				GUI.progressBarManager.error(progressBarId, GUI.translate("Upload aborted"));
+			},false);
+			
+			xhr.open("POST", "/setContent/"+newObject.getCurrentRoom()+"/"+newObject.getAttribute('id')+"/"+ObjectManager.userHash);
+			xhr.send(fd);
+		
+		
+			GUI.progressBarManager.updateProgress(progressBarId, 0, GUI.translate("Upload file"));
+			
+			newObject.setAttribute('mimeType',mimeType);
+			newObject.setAttribute('name',filename);
+			
+		
+			
+		});
+				
+		}
+		
 		if (files != undefined && files != null && files.length > 0) {
 			/* files dropped */
 
 			for (var i=0; i < files.length; i++) {
-				
-				var progressBarId = GUI.progressBarManager.addProgress(GUI.translate("Create file object"));
-				
-				var filename = files[i].fileName;
-				var mimeType = files[i].type;
-				var file = files[i];
 
-				/* create new File object and set position */
-				ObjectManager.createObject("File",{
-					"x":x,
-					"y":y,
-					"hasContent":true //prevent calling justCreated() after object creation (would display file upload dialog)
-				},false,function(newObject) {
-					/* object created --> upload content */
-					
-					var fd=new FormData();
-					fd.append("file", file); // Append the file
-					
-					var xhr = new XMLHttpRequest();
-					
-					xhr.upload.addEventListener("progress",function(evt) {
-						if (evt.lengthComputable){
-							
-							var percentComplete=Math.round(evt.loaded * 100 / evt.total);
-							GUI.progressBarManager.updateProgress(progressBarId, percentComplete);
-							
-						}
-					}, false);
-					
-					xhr.addEventListener("load",function() {
-						GUI.progressBarManager.updateProgress(progressBarId, 100, GUI.translate("Upload completed"), true);
-					},false);
-					
-					xhr.addEventListener("error",function() {
-						GUI.progressBarManager.error(progressBarId, GUI.translate("Error while uploading file"));
-					},false);
-					
-					xhr.addEventListener("abort",function() {
-						GUI.progressBarManager.error(progressBarId, GUI.translate("Upload aborted"));
-					},false);
-					
-					xhr.open("POST", "/setContent/"+newObject.getCurrentRoom()+"/"+newObject.getAttribute('id')+"/"+ObjectManager.userHash);
-					xhr.send(fd);
-				
-				
-					GUI.progressBarManager.updateProgress(progressBarId, 0, GUI.translate("Upload file"));
-				
-					
-				});
+				upload(files[i]);
 				
 			}
-			
 		}
 		
 	}
