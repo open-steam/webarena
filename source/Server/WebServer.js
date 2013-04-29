3282524457e2f47ef88a47f9a8537a657b822e12
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2013-04-29 16:35:22 +0200

    Mime type for icons

--- source/Server/WebServer.js
+++ source/Server/WebServer.js
@@ -448,6 +448,7 @@ WebServer.init=function(theModules){
 					if (url.indexOf('.htm')!=-1) contentType='text/html';
 					if (url.indexOf('.js')!=-1) contentType='application/javascript';
 					if (url.indexOf('.css')!=-1) contentType='text/css';
+					if (url.indexOf('.ico')!=-1) contentType='image/x-icon';
 
 					if (!contentType) {
 						Modules.Log.warn('WebServer ERROR: No content type for '+url);
