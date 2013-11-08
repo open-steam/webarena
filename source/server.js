71bb62cb0e5aa01d95f372b63aee961180d7216d
Dominik Lüke <lucky@upb.de>
2013-11-08 13:19:24 +0100

    undo uncomment

--- source/server.js
+++ source/server.js
@@ -31,11 +31,9 @@ var printLogo = function(){
 //General error handling. Let the server try to continue
 //if an error occured and log the error
 
-/*
 process.on('uncaughtException', function (err) {
 	Modules.Log.error(err);
 });
-*/
 
 //Loading the configuration. Entires in config.local.js overlap those in config.default.js
 
