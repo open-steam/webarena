07afac0c526ab86b44a18895c6b431026b69cfa7
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2013-11-15 11:00:50 +0100

    Fixed display of uncaught exception

--- source/server.js
+++ source/server.js
@@ -31,7 +31,8 @@ var printLogo = function(){
 //if an error occured and log the error
 
 process.on('uncaughtException', function (err) {
-	Modules.Log.error(err);
+	console.log('##### UNCAUGHT EXCEPTTION');
+	console.log(err);
 });
 
 //Loading the configuration. Entires in config.local.js overlap those in config.default.js
