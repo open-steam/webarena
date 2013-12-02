65b8317b19f20bb4b4ba2c0363de2bceaa7f4468
Viktor Koop <viktor.koop@gmail.com>
2013-12-02 11:12:02 +0100

    cleanup: add some missing semicolons

--- source/Server/HistoryTracker.js
+++ source/Server/HistoryTracker.js
@@ -3,13 +3,13 @@
 var _ = require("lodash");
 
 var HistoryTracker = function(maxStackSize){
-    var that = {}
+    var that = { };
 
     /**
      * The ids of the stored transactions. Should be ordered by time.
      * @type {Array}
      */
-    var historyTransactionIds = []
+    var historyTransactionIds = [];
 
     /**
      * For each history entry there should exist an object attribute,
@@ -20,7 +20,7 @@ var HistoryTracker = function(maxStackSize){
      */
     var historyEntries = {
 
-    }
+    };
 
     /**
      * The history stack should have a limited size. Therefore it should be shortened
