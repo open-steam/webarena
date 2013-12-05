66babef63fc28b068844ff54ea1f7b1273bd07e4
Viktor Koop <viktor.koop@gmail.com>
2013-12-05 15:07:31 +0100

    tcp dispatcher set event delimiter

--- source/Server/apihandler/TcpDispatcher.js
+++ source/Server/apihandler/TcpDispatcher.js
@@ -7,39 +7,38 @@ var Modules = false;
 var EventEmitter2 = require('eventemitter2').EventEmitter2;
 
 var TcpDispatcher = new EventEmitter2({
-	wildcard: true
+    wildcard: true,
+    delimiter: "::"
 });
-var _ = require("lodash");
 
 TcpDispatcher.init = function (theModules) {
-	Modules = theModules;
+    Modules = theModules;
 }
 
 TcpDispatcher.on("subscribeEvents", function (event) {
-	var toSubscribe = event.eventlist;
+    var toSubscribe = event.eventlist;
 
-	if (!toSubscribe) {
-		return event.connection.sendMessage({error: "Missing eventlist"});
-	}
+    if (!toSubscribe) {
+        return event.connection.sendMessage({error: "Missing eventlist"});
+    }
 
-	if (!_.isArray(toSubscribe)) {
-		toSubscribe = [toSubscribe];
-	}
+    if (!util.isArray(toSubscribe)) {
+        toSubscribe = [toSubscribe];
+    }
 
-	toSubscribe.forEach(function (eventParam) {
-		Modules.EventBus.on(eventParam, function (eventData) {
+    toSubscribe.forEach(function (eventParam) {
+        Modules.EventBus.on(eventParam, function (eventData) {
 
-			var eventEnvelope = {
-				eventName: this.event,
-				eventData: eventData
-			};
+            var eventEnvelope = {
+                eventName: this.event,
+                eventData: eventData
+            };
 
-			event.connection.sendMessage(eventEnvelope);
-		});
-	});
+            event.connection.sendMessage(eventEnvelope);
+        });
+    });
 
-	event.connection.sendMessage({status: "ok"});
-
+    event.connection.sendMessage({status: "ok"});
 });
 
 
