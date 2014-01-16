99672eabc05605e865e488481458a8a040e0f735
Viktor Koop <viktor.koop@gmail.com>
2014-01-16 16:19:02 +0100

    Object transports don’t depend on hotspot anymore.

--- source/objects/8.ObjectTransport/common.js
+++ source/objects/8.ObjectTransport/common.js
@@ -4,7 +4,7 @@ var ObjectTransport=Object.create(Modules.ObjectManager.getPrototype('Hotspot'))
 ObjectTransport.register=function(type){
 
 	// Registering the object
-	Modules.ObjectManager.getPrototype('Hotspot').register.call(this,type);
+	Modules.ObjectManager.getPrototype('GeneralObject').register.call(this,type);
 	this.makeSensitive();
 	this.registerAttribute('target',{type:'text',standard:'',category:'Selection'});
 	this.registerAttribute('cut',{type:'boolean',standard:false, category:'Selection'});
@@ -17,6 +17,7 @@ ObjectTransport.execute=function(){
 }
 
 ObjectTransport.isCreatable=true;
+ObjectTransport.isResizable = function(){ return true;};
 ObjectTransport.category = 'Active';
 
 
