d982f3182efd6d3a756853f3ebb1616325a89368
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2014-04-01 18:29:11 +0200

    Testcommit

--- source/objects/Base/1.GeneralObject/view.js
+++ source/objects/Base/1.GeneralObject/view.js
@@ -1,7 +1,7 @@
 /**
 *    Webarena - A web application for responsive graphical knowledge work
 *
-*    @author Felix Winkelnkemper, University of Paderborn, 2012
+*    @author Felix Winkelnkemper, University of Paderborn, 2014
 *
 *	 GeneralObject view component
 *
