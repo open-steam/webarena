e9b0137f0c860890b5850ceeb3835086fc345224
Christoph Sens <csens@mail.upb.de>
2014-11-05 23:55:50 +0100

    added 'position => value' logic

--- source/objects/Responsive/8.Matrix/server.js
+++ source/objects/Responsive/8.Matrix/server.js
@@ -12,73 +12,83 @@ var Modules = require('../../../server.js');
 module.exports = theObject;
 
 theObject.onLeave = function(object, data) {
-
-    /*var attributeName = this.getAttribute('attribute');
-    if (attributeName !== '') {
-        object.setAttribute(attributeName, '');
-        
-        //TODO:reduce to a function start
-        var direction = this.getAttribute('direction');
-        var overlappingObjects = this.getOverlappingObjects();
-
-        var sortedPositions = [];
-
-        for (var index in overlappingObjects) {
-            if (overlappingObjects[index].isActive && overlappingObjects[index].isActive) {
-                sortedPositions.push(overlappingObjects[index].getAttribute(direction));
-            }
-
-        }
-        sortedPositions = sortedPositions.sort();
-
-        var counter = 1;
-        for (var i in sortedPositions) {
-            for (var index in overlappingObjects) {
-                if (overlappingObjects[index].getAttribute(direction) === sortedPositions[i]) {
-                    console.log(overlappingObjects[index].getAttribute('id') + "    " + counter);
-                    overlappingObjects[index].setAttribute(this.getAttribute('attribute'), counter);
-                    counter++;
-                    break;
-                }
-            }
-        }
-//TODO:reduce to a function end
-    } else {
-        console.log("List: No attribute set for " + object.getAttribute('id'));
-    } */
-
-
+    if (object.getAttribute('Row-' + this.id) !== undefined 
+            && object.getAttribute('Row-' + this.id) !== ""
+            && object.getAttribute('Column-' + this.id) !== undefined
+            && object.getAttribute('Column-' + this.id) !== "") {
+        object.setAttribute('Row-' + this.id, "");
+        object.setAttribute('Column-' + this.id, "");
+        console.log("removed attributes");
+    }
 };
 
-
 theObject.onEnter = function(object, data) {
+    //data is undefined
+    //obj is the current object which was move on "this" structure
+    var structureWidth = this.getAttribute('width');
+    var structureHeigth = this.getAttribute('height');
+    var rows = this.getAttribute('Row');
+    var columns = this.getAttribute('Column');
+    var cellWidth = structureWidth / (columns.length + 1);
+    var cellHeight = structureHeigth / (rows.length + 1);
+
+    var objCX = object.getAttribute("cx");
+    var objCY = object.getAttribute("cy");
+
+    var relativeX = objCX - this.getAttribute('x');
+    var relativeY = objCY - this.getAttribute('y');
+
+    if ((relativeX >= cellWidth) && (relativeY >= cellHeight)) {
+        var xIndex = Math.floor(relativeX / cellWidth);
+        var yIndex = Math.floor(relativeY / cellHeight);
+
+        var attrX = columns[xIndex - 1];
+        var attrY = rows[yIndex - 1];
+
+        var currentValueX = object.getAttribute('Row-' + this.id);
+        var currentValueY = object.getAttribute('Column-' + this.id);
+
+        if (currentValueX !== attrX && currentValueY !== attrY) {
+            object.setAttribute('Row-' + this.id, attrX);
+            object.setAttribute('Column-' + this.id, attrY);
+        } else {
+            console.log("attributes already set!");
+        }
+
+
+    } else {
+        console.log("Inside the structure, but outside the value area");
+        object.setAttribute('Row-' + this.id, "");
+        object.setAttribute('Column-' + this.id, "");
+    }
+
     //TODO:reduce to a function start
-    
-   /* var direction = this.getAttribute('direction');
-    var overlappingObjects = this.getOverlappingObjects();
 
-    var sortedPositions = [];
-
-    for (var index in overlappingObjects) {
-        if (overlappingObjects[index].isActive && overlappingObjects[index].isActive) {
-            sortedPositions.push(overlappingObjects[index].getAttribute(direction));
-        }
-
-    }
-    sortedPositions = sortedPositions.sort();
-
-    var counter = 1;
-    for (var i in sortedPositions) {
-        for (var index in overlappingObjects) {
-            if (overlappingObjects[index].getAttribute(direction) === sortedPositions[i]) {
-                console.log(overlappingObjects[index].getAttribute('id') + "    " + counter);
-                overlappingObjects[index].setAttribute(this.getAttribute('attribute'), counter);
-                counter++;
-                break;
-            }
-        }
-    }
-    //TODO:reduce to a function end */
+    /* var direction = this.getAttribute('direction');
+     var overlappingObjects = this.getOverlappingObjects();
+     
+     var sortedPositions = [];
+     
+     for (var index in overlappingObjects) {
+     if (overlappingObjects[index].isActive && overlappingObjects[index].isActive) {
+     sortedPositions.push(overlappingObjects[index].getAttribute(direction));
+     }
+     
+     }
+     sortedPositions = sortedPositions.sort();
+     
+     var counter = 1;
+     for (var i in sortedPositions) {
+     for (var index in overlappingObjects) {
+     if (overlappingObjects[index].getAttribute(direction) === sortedPositions[i]) {
+     console.log(overlappingObjects[index].getAttribute('id') + "    " + counter);
+     overlappingObjects[index].setAttribute(this.getAttribute('attribute'), counter);
+     counter++;
+     break;
+     }
+     }
+     }
+     //TODO:reduce to a function end */
 
 };
 
