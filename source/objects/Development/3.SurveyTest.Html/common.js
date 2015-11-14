fc75430a3fd8bd52fe82fd1593cd9193a90dffd2
Lutz Terfloth <lutzt@mail.upb.de>
2015-11-14 14:13:39 +0100

    - Added a basic survey-template for development purposes with corresponding attributes - Added sliderChange function (still flawed) - overwrote unmakeMoveable function, because it caused a bug

--- source/objects/Development/3.SurveyTest.Html/common.js
+++ source/objects/Development/3.SurveyTest.Html/common.js
@@ -10,17 +10,30 @@ var Modules=require('../../../server.js');
 var SurveyTest=Object.create(Modules.ObjectManager.getPrototype('HtmlObject'));
 
 SurveyTest.register=function(type){
-	
+
 	// Registering the object
-	
+
 	HtmlObject=Modules.ObjectManager.getPrototype('HtmlObject');
 	HtmlObject.register.call(this,type);
-	
+
+	this.registerAttribute('points_1',{hidden:false, type: 'number', min: -5, standard: 0, max: 5});
+	this.registerAttribute('points_2',{hidden:false, type: 'number', min: -5, standard: 0, max: 5});
+	this.registerAttribute('points_3',{hidden:false, type: 'number', min: -5, standard: 0, max: 5});
 }
+
+SurveyTest.sliderChange = function(sliderID, value){
+	var slider = 'slider_'+sliderID;
+	var display = 'display_'+sliderID;
+	var attribute = 'points_'+sliderID;
+	var value = value;
+
+	//$(display).value = value;
+	this.setAttribute(attribute, value);
+}
+
 
 SurveyTest.register('SurveyTest');
 SurveyTest.isCreatable=true;
-
 SurveyTest.contentURLOnly = false;
 
 //set restrictedMovingArea to true, if you want to enable interface interaction within
