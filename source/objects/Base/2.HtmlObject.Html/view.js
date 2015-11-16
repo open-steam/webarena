40c356ef77b4f514b29de18a33d093e36e446e6f
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2015-11-16 15:12:37 +0100

    Added getArenaObject to generalObject which tries to find the ArenaObject which belongs to a graphical object Used this to fix an error where the object context cannot be determined TODO: Check why those event functions are called on the wrong context!

--- source/objects/Base/2.HtmlObject.Html/view.js
+++ source/objects/Base/2.HtmlObject.Html/view.js
@@ -61,20 +61,7 @@ HtmlObject.createRepresentation = function(parent) {
 }
 
 
-/**
-*   getArenaObject
-*
-*	gets the WebArenaObject for a given htmlobject by going through the dom structure
-*/
-HtmlObject.getArenaObject=function(htmlobject){
-	
-	if (!htmlobject) return undefined;
-	
-	if (htmlobject.dataObject) return htmlobject.dataObject;
-	
-	return this.getArenaObject(htmlobject.parentNode);
-	
-}
+
 
 /**
 *
