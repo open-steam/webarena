08d9d98b8559f87db16c1bacc1e6359ffbbdea00
Felix Winkelnkemper <winkelnkemper@googlemail.com>
2015-11-19 17:12:50 +0100

    Checking in the working and improved user management from 2014

--- usertool/application/config/platform.php
+++ usertool/application/config/platform.php
@@ -1,19 +1,11 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['admins'] = array("csens");
-$config['webarenaHost'] = "localhost";
+$config['admins'] = array("winfel",'janp','mjako');
+$config['webarenaHost'] = "sulley.cs.uni-paderborn.de";
 $config['webarenaPort'] = "8080";
-$config['dataFolder'] = "/Users/christophsens/waData/";
+$config['dataFolder'] = "/Users/felixwinkelnkemper/wa_userdata";
 
 $config['userFolder'] = $config['dataFolder'] . "/users";
 $config['courseFolder'] = $config['dataFolder'] . "/courses";
-
-
-defined("PLATTFORM_ID") ||
-    define("PLATTFORM_ID", "a");
-
-defined("LOGIN_WITH_EMAIL") ||
-    define("LOGIN_WITH_EMAIL", FALSE);
-
 
 ?>
